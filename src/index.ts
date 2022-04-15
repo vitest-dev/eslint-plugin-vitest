c9e097a242b0f3c150cb8fd3094bae47d5b7bbe1
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-04-15 12:45:55 +0200

    fix: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import assertionType from "./rules/assertion-type";
 import lowerCaseTitle from "./rules/lower-case-title";
+import maxNestedDescribe from "./rules/max-nested-describe";
+import noConditionalInTest from "./rules/no-conditional-in-tests";
+import noIdenticalTitle from "./rules/no-identical-title";
 import noSkippedTests from "./rules/no-skipped-tests";
 
 export default {
@@ -7,5 +10,8 @@ export default {
     "no-skip-test": noSkippedTests,
     "lower-case-title": lowerCaseTitle,
     "assertion-type": assertionType,
+    "max-nested-describe": maxNestedDescribe,
+    "no-idential-title": noIdenticalTitle,
+    "no-conditional-in-test": noConditionalInTest,
   },
 };
