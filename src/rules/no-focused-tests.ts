5ec7bfa071321a13357850555e3e0392450cdb84
Ev Haus <ev@haus.gg>
2022-11-06 18:00:49 -0800

    Add new no-focused-tests rule

---
+++ src/rules/no-focused-tests.ts
@@ -0,0 +1,43 @@
+import { createEslintRule } from "../utils";
+
+export type MessageIds = "noFocusedTests";
+export const RULE_NAME = "no-focused-tests";
+export type Options = [];
+
+export default createEslintRule<Options, MessageIds>({
+  name: RULE_NAME,
+  meta: {
+    type: "problem",
+    docs: {
+      description: "Disallow focused tests",
+      recommended: "error",
+    },
+    fixable: "code",
+    schema: [],
+    messages: {
+        noFocusedTests: "Focused tests are not allowed.",
+    },
+  },
+  defaultOptions: [],
+  create: (context) => {
+    return {
+      ExpressionStatement(node) {
+        if (node.expression.type === "CallExpression") {
+          const { callee } = node.expression;
+          if (
+            callee.type === "MemberExpression" &&
+            callee.object.type === "Identifier" &&
+            (callee.object.name === "it" || callee.object.name === "describe") &&
+            callee.property.type === "Identifier" &&
+            callee.property.name === "only"
+          ) {
+            context.report({
+              node,
+              messageId: "noFocusedTests",
+            });
+          }
+        }
+      },
+    };
+  },
+});
