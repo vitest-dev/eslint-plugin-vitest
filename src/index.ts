801fba9f10ff928df91e742e918a3a11ac8907ac
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-11-10 15:13:30 +0200

    chore: remove unfinished rules

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
-import assertionType, {RULE_NAME as assertionTypeName} from "./rules/assertion-type";
-import lowerCaseTitle, {RULE_NAME as lowerCaseTitleName} from "./rules/lower-case-title";
-import maxNestedDescribe, {RULE_NAME as maxNestedDescribeName} from "./rules/max-nested-describe";
-import noConditionalInTest, {RULE_NAME as noConditionalInTestName} from "./rules/no-conditional-in-tests";
-import noIdenticalTitle, {RULE_NAME as noIdenticalTitleName} from "./rules/no-identical-title";
-import noSkippedTests, {RULE_NAME as noSkippedTestsName} from "./rules/no-skipped-tests";
-import noFocusedTests, {RULE_NAME as noFocusedTestsName} from "./rules/no-focused-tests";
+import lowerCaseTitle, { RULE_NAME as lowerCaseTitleName } from "./rules/lower-case-title";
+import maxNestedDescribe, { RULE_NAME as maxNestedDescribeName } from "./rules/max-nested-describe";
+import noIdenticalTitle, { RULE_NAME as noIdenticalTitleName } from "./rules/no-identical-title";
+import noSkippedTests, { RULE_NAME as noSkippedTestsName } from "./rules/no-skipped-tests";
+import noFocusedTests, { RULE_NAME as noFocusedTestsName } from "./rules/no-focused-tests";
 
 export default {
-  rules: {
-    [noSkippedTestsName]: noSkippedTests,
-    [lowerCaseTitleName]: lowerCaseTitle,
-    [assertionTypeName]: assertionType,
-    [maxNestedDescribeName]: maxNestedDescribe,
-    [noIdenticalTitleName]: noIdenticalTitle,
-    [noConditionalInTestName]: noConditionalInTest,
-    [noFocusedTestsName]: noFocusedTests,
-  },
+	rules: {
+		[noSkippedTestsName]: noSkippedTests,
+		[lowerCaseTitleName]: lowerCaseTitle,
+		[maxNestedDescribeName]: maxNestedDescribe,
+		[noIdenticalTitleName]: noIdenticalTitle,
+		[noFocusedTestsName]: noFocusedTests,
+	},
 };
