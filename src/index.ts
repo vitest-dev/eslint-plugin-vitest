7c83e700db173bbb0ae15b8b12ec29a841fd2363
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-02-11 21:48:19 +0200

    feat: adding globals

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { readdirSync } from 'fs'
+import { join } from 'path'
 import lowerCaseTitle, { RULE_NAME as lowerCaseTitleName } from './rules/prefer-lowercase-title'
 import maxNestedDescribe, { RULE_NAME as maxNestedDescribeName } from './rules/max-nested-describe'
 import noIdenticalTitle, { RULE_NAME as noIdenticalTitleName } from './rules/no-identical-title'
@@ -7,6 +9,36 @@ import noConditionalTest, { RULE_NAME as noConditionalTests } from './rules/no-c
 import expectExpect, { RULE_NAME as expectedExpect } from './rules/expect-expect'
 import consistentTestIt, { RULE_NAME as useConsistentTestIt } from './rules/consistent-test-it'
 import preferToBe, { RULE_NAME as usePreferTobe } from './rules/prefer-to-be'
+
+const rulesDir = join(__dirname, 'rules')
+
+const rules = readdirSync(rulesDir)
+	.filter((file) => !file.includes('.test.ts') && file !== 'index.ts')
+	.reduce((acc, curr) => ({
+		...acc,
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
+		[curr]: require(join(rulesDir, curr)).default
+	}), {})
+
+const allRules = Object.keys(rules)
+	.map(rule => rule.replace(/\.ts$/, ''))
+	.reduce((acc, curr) => ({
+		...acc,
+		[`vitest/${curr}`]: 'error'
+	}), {})
+
+const recommended = Object.keys(rules)
+	.filter((rule) => rules[rule].meta.docs.recommended === 'strict')
+	.map(rule => rule.replace(/\.ts$/, ''))
+	.reduce((acc, curr) => ({
+		...acc,
+		[`vitest/${curr}`]: 'error'
+	}), {})
+
+const createConfig = (rules) => ({
+	plugins: ['vitest'],
+	rules
+})
 
 export default {
 	rules: {
@@ -19,5 +51,9 @@ export default {
 		[expectedExpect]: expectExpect,
 		[useConsistentTestIt]: consistentTestIt,
 		[usePreferTobe]: preferToBe
+	},
+	configs: {
+		all: createConfig(allRules),
+		recommended: createConfig(recommended)
 	}
 }
