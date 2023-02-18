0f42538b1df367a6fa3471a27282ee38c3a18f0d
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-02-18 23:52:41 +0200

    chore: updated

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,6 @@ import noHooks, { RULE_NAME as noHooksName } from './rules/no-hooks'
 
 const createConfig = (rules: Record<string, string>) => ({
 	plugins: ['vitest'],
-	env: {
-		suite: true,
-		test: true,
-		describe: true,
-		it: true,
-		expect: true,
-		assert: true,
-		vitest: true,
-		vi: true,
-		beforeAll: true,
-		afterAll: true,
-		beforeEach: true,
-		afterEach: true
-	},
 	rules: Object.keys(rules).reduce((acc, ruleName) => {
 		return {
 			...acc,
@@ -65,5 +51,23 @@ export default {
 	configs: {
 		all: createConfig(allRules),
 		recommended: createConfig(recommended)
+	},
+	environments: {
+		env: {
+			globals: {
+				suite: true,
+				test: true,
+				describe: true,
+				it: true,
+				expect: true,
+				assert: true,
+				vitest: true,
+				vi: true,
+				beforeAll: true,
+				afterAll: true,
+				beforeEach: true,
+				afterEach: true
+			}
+		}
 	}
 }
