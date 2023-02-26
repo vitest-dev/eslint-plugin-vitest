58c3f2164e5ddef1a824cb8c3bacd29a948f82bf
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-02-26 23:44:03 +0200

    feat: added a new rule

---
+++ docs/rules/no-commented-out-tests.md
@@ -0,0 +1,25 @@
+# Disallow commented out tests (`vitest/no-commented-out-tests`)
+
+⚠️ This rule _warns_ in the ✅ `recommended` config.
+
+<!-- end auto-generated rule header -->
+
+## Rule Details
+
+This rule aims to prevent commented out tests.
+
+Examples of **incorrect** code for this rule:
+
+```ts
+// test('foo', () => {
+//   expect(1).toBe(1)
+// })
+```
+
+Examples of **correct** code for this rule:
+
+```ts
+test('foo', () => {
+  expect(1).toBe(1)
+})
+```
---
+++ src/rules/no-commented-out-tests.ts
@@ -0,0 +1,45 @@
+import { TSESTree } from '@typescript-eslint/utils'
+import { createEslintRule } from '../utils'
+
+export const RULE_NAME = 'no-commented-out-tests'
+export type MESSAGE_IDS = 'noCommentedOutTests';
+export type Options = [];
+
+function hasTests(node: TSESTree.Comment) {
+	return /^\s*[xf]?(test|it|describe)(\.\w+|\[['"]\w+['"]\])?\s*\(/mu.test(
+		node.value
+	)
+}
+
+export default createEslintRule<Options, MESSAGE_IDS>({
+	name: RULE_NAME,
+	meta: {
+		docs: {
+			description: 'Disallow commented out tests',
+			requiresTypeChecking: false,
+			recommended: 'warn'
+		},
+		messages: {
+			noCommentedOutTests: 'Remove commented out tests'
+		},
+		schema: [],
+		type: 'suggestion'
+	},
+	defaultOptions: [],
+	create(context) {
+		const sourceCode = context.getSourceCode()
+
+		function checkNodeForCommentedOutTests(node: TSESTree.Comment) {
+			if (!hasTests(node))
+				return
+			context.report({ messageId: 'noCommentedOutTests', node })
+		}
+
+		return {
+			Program() {
+				const comments = sourceCode.getAllComments()
+				comments.forEach(checkNodeForCommentedOutTests)
+			}
+		}
+	}
+})
