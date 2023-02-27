0a26d87b860ccbddbba7ede6d49a0d1b2c7c5123
Verité Mugabo <mugaboverite@gmail.com>
2023-02-27 09:27:29 +0200

    feat: no-disabled-tests (#70)

---
+++ src/rules/no-disabled-tests.ts
@@ -0,0 +1,83 @@
+import { createEslintRule, getAccessorValue } from '../utils'
+import { parseVitestFnCall, resolveScope } from '../utils/parseVitestFnCall'
+
+export const RULE_NAME = 'no-disabled-tests'
+export type MESSAGE_ID = 'missingFunction' | 'pending' | 'pendingSuite' | 'pendingTest' | 'disabledSuite' | 'disabledTest';
+export type Options = [];
+
+export default createEslintRule<Options, MESSAGE_ID>({
+	name: RULE_NAME,
+	meta: {
+		type: 'suggestion',
+		docs: {
+			description: 'Disallow disabled tests',
+			recommended: false
+		},
+		messages: {
+			missingFunction: 'Test is missing function argument',
+			pending: 'Call to pending()',
+			pendingSuite: 'Call to pending() within test suite',
+			pendingTest: 'Call to pending() within test',
+			disabledSuite: 'Disabled test suite',
+			disabledTest: 'Disabled test'
+		},
+		schema: []
+	},
+	defaultOptions: [],
+	create(context) {
+		let suiteDepth = 0
+		let testDepth = 0
+
+		return {
+			CallExpression(node) {
+				const vitestFnCall = parseVitestFnCall(node, context)
+
+				if (!vitestFnCall) return
+
+				if (vitestFnCall.type === 'describe')
+					suiteDepth++
+
+				if (vitestFnCall.type === 'test') {
+					testDepth++
+
+					if (node.arguments.length < 2 && vitestFnCall.members.every(s => getAccessorValue(s) === 'skip')) {
+						context.report({
+							messageId: 'missingFunction',
+							node
+						})
+					}
+				}
+
+				if (vitestFnCall.name.startsWith('x') || vitestFnCall.members.some(s => getAccessorValue(s) === 'skip')) {
+					context.report({
+						messageId: vitestFnCall.type === 'describe' ? 'disabledSuite' : 'disabledTest',
+						node
+					})
+				}
+			},
+			'CallExpression:exit'(node) {
+				const vitestFnCall = parseVitestFnCall(node, context)
+
+				if (!vitestFnCall)
+					return
+
+				if (vitestFnCall.type === 'describe')
+					suiteDepth--
+
+				if (vitestFnCall.type === 'test')
+					testDepth--
+			},
+			'CallExpression[callee.name="pending"]'(node) {
+				if (resolveScope(context.getScope(), 'pending'))
+					return
+
+				if (testDepth > 0)
+					context.report({ messageId: 'pendingTest', node })
+				else if (suiteDepth > 0)
+					context.report({ messageId: 'pendingSuite', node })
+				else
+					context.report({ messageId: 'pending', node })
+			}
+		}
+	}
+})
