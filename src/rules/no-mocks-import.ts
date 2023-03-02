9b2c3c0f225e5528e1de40525bc8bf74e5ae8e6c
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-03-02 10:01:21 +0200

    fix: updated

---
+++ src/rules/no-mocks-import.ts
@@ -0,0 +1,43 @@
+import { posix } from 'node:path'
+import { TSESTree } from '@typescript-eslint/utils'
+import { createEslintRule, getStringValue, isStringNode } from '../utils'
+
+const mocksDirName = '__mocks__'
+
+const isMockPath = (path: string) => path.split(posix.sep).includes(mocksDirName)
+
+const isMockImportLiteral = (expression: TSESTree.CallExpressionArgument) => isStringNode(expression) && isMockPath(getStringValue(expression))
+
+export const RULE_NAME = 'no-mocks-import'
+type MESSAGE_IDS = 'noMocksImport';
+type Options = []
+
+export default createEslintRule<Options, MESSAGE_IDS>({
+	name: RULE_NAME,
+	meta: {
+		type: 'problem',
+		docs: {
+			description: 'Disallow importing from __mocks__ directory',
+			recommended: 'error'
+		},
+		messages: {
+			noMocksImport: `Mocks should not be manually imported from a ${mocksDirName} directory. Instead use \`jest.mock\` and import from the original module path.`
+		},
+		schema: []
+	},
+	defaultOptions: [],
+	create(context) {
+		return {
+			ImportDeclaration(node: TSESTree.ImportDeclaration) {
+				if (isMockImportLiteral(node.source))
+					context.report({ node, messageId: 'noMocksImport' })
+			},
+			'CallExpression[callee.name="require"]'(node: TSESTree.CallExpression) {
+				const [args] = node.arguments
+
+				if (args && isMockImportLiteral(args))
+					context.report({ node: args, messageId: 'noMocksImport' })
+			}
+		}
+	}
+})
