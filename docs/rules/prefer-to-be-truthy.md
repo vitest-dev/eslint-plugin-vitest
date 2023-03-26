19307b352369fb6fb1220cbefafb7c1eb207e7d6
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-03-26 11:13:13 +0200

    feat: updated

---
+++ docs/rules/prefer-to-be-truthy.md
@@ -0,0 +1,17 @@
+# Suggest using `toBeTruthy` (`vitest/prefer-to-be-truthy`)
+
+⚠️ This rule _warns_ in the 🌐 `all` config.
+
+🔧 This rule is automatically fixable by the [`--fix` CLI option](https://eslint.org/docs/latest/user-guide/command-line-interface#--fix).
+
+<!-- end auto-generated rule header -->
+
+```js
+// bad
+expect(foo).toBe(true)
+expectTypeOf(foo).toBe(true)
+
+// good
+expect(foo).toBeTruthy()
+expectTypeOf(foo).toBeTruthy()
+```
---
+++ src/rules/prefer-to-be-truthy.ts
@@ -0,0 +1,55 @@
+import { AST_NODE_TYPES, TSESTree } from '@typescript-eslint/utils'
+import { createEslintRule, getAccessorValue } from '../utils'
+import { getFirstMatcherArg, parseVitestFnCall } from '../utils/parseVitestFnCall'
+import { EqualityMatcher } from '../utils/types'
+
+type MESSAGE_IDS = 'preferToBeTruthy'
+export const RULE_NAME = 'prefer-to-be-truthy'
+type Options = []
+
+interface TrueLiteral extends TSESTree.BooleanLiteral {
+	value: true;
+}
+
+const isTrueLiteral = (node: TSESTree.Node): node is TrueLiteral =>
+	node.type === AST_NODE_TYPES.Literal && node.value === true
+
+export default createEslintRule<Options, MESSAGE_IDS>({
+	name: RULE_NAME,
+	meta: {
+		type: 'suggestion',
+		docs: {
+			description: 'Suggest using `toBeTruthy`',
+			recommended: 'warn'
+		},
+		messages: {
+			preferToBeTruthy: 'Prefer using `toBeTruthy` to test value is `true`'
+		},
+		fixable: 'code',
+		schema: []
+	},
+	defaultOptions: [],
+	create(context) {
+		return {
+			CallExpression(node) {
+				const vitestFnCall = parseVitestFnCall(node, context)
+
+				if (!(vitestFnCall?.type === 'expect' || vitestFnCall?.type === 'expectTypeOf')) return
+
+				if (vitestFnCall.args.length === 1 &&
+					isTrueLiteral(getFirstMatcherArg(vitestFnCall)) &&
+					// eslint-disable-next-line no-prototype-builtins
+					EqualityMatcher.hasOwnProperty(getAccessorValue(vitestFnCall.matcher))) {
+					context.report({
+						node: vitestFnCall.matcher,
+						messageId: 'preferToBeTruthy',
+						fix: fixer => [
+							fixer.replaceText(vitestFnCall.matcher, 'toBeTruthy'),
+							fixer.remove(vitestFnCall.args[0])
+						]
+					})
+				}
+			}
+		}
+	}
+})
