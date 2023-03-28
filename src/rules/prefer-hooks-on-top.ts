eb9887a4398ae01fcd7ff5a1e0f35fb775880b19
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-03-29 01:37:27 +0200

    feat: updated

---
+++ src/rules/prefer-hooks-on-top.ts
@@ -0,0 +1,43 @@
+import { createEslintRule } from '../utils'
+import { isTypeOfVitestFnCall } from '../utils/parseVitestFnCall'
+
+export const RULE_NAME = 'prefer-hooks-on-top'
+type MessageIds = 'noHookOnTop';
+type Options = [];
+
+export default createEslintRule<Options, MessageIds>({
+	name: RULE_NAME,
+	meta: {
+		type: 'suggestion',
+		docs: {
+			description: 'Suggest having hooks before any test cases',
+			recommended: 'error'
+		},
+		messages: {
+			noHookOnTop: 'Hooks should come before test cases'
+		},
+		schema: []
+	},
+	defaultOptions: [],
+	create(context) {
+		const hooksContext = [false]
+		return {
+			CallExpression(node) {
+				if (isTypeOfVitestFnCall(node, context, ['test']))
+					hooksContext[hooksContext.length - 1] = true
+
+				if (hooksContext[hooksContext.length - 1] && isTypeOfVitestFnCall(node, context, ['hook'])) {
+					context.report({
+						messageId: 'noHookOnTop',
+						node
+					})
+				}
+
+				hooksContext.push(false)
+			},
+			'CallExpression:exit'() {
+				hooksContext.pop()
+			}
+		}
+	}
+})
