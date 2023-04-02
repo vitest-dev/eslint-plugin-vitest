b1ababeb1ae28c900280d24914873b3ca8c1313d
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-04-03 01:37:16 +0200

    feat: updated

--- src/rules/valid-expect-in-promise.ts
+++ src/rules/valid-expect-in-promise.ts
@@ -1,10 +1,14 @@
 import { AST_NODE_TYPES, TSESLint, TSESTree } from '@typescript-eslint/utils'
-import { createEslintRule, getAccessorValue, isFunction } from '../utils'
-import { parseVitestFnCall } from '../utils/parseVitestFnCall'
+import { KnownCallExpression, createEslintRule, getAccessorValue, isFunction, isSupportedAccessor } from '../utils'
+import { isTypeOfVitestFnCall, parseVitestFnCall } from '../utils/parseVitestFnCall'
 
 export const RULE_NAME = 'valid-expect-in-promise'
 type MESSAGE_IDS = 'expectInFloatingPromise'
 type Options = []
+
+type PromiseChainCallExpression = KnownCallExpression<
+	'then' | 'catch' | 'finally'
+>;
 
 const isTestCaseCallWithCallbackArg = (node: TSESTree.CallExpression, context: TSESLint.RuleContext<string, Options>): boolean => {
 	const vitesCallFn = parseVitestFnCall(node, context)
@@ -22,6 +26,25 @@ const isTestCaseCallWithCallbackArg = (node: TSESTree.CallExpression, context: T
 	const callbackArgIndex = Number(isVitestEach)
 
 	return callback && isFunction(callback) && callback.params.length === 1 + callbackArgIndex
+}
+
+const isPromiseChainCall = (node: TSESTree.Node): node is PromiseChainCallExpression => {
+	if (node.type === AST_NODE_TYPES.CallExpression &&
+		node.callee.type === AST_NODE_TYPES.MemberExpression &&
+		isSupportedAccessor(node.callee.property)) {
+		if (node.arguments.length === 0)
+			return false
+
+		switch (getAccessorValue(node.callee.property)) {
+			case 'then':
+				return node.arguments.length < 3
+			case 'catch':
+			case 'finally':
+				return node.arguments.length < 2
+		}
+	}
+
+	return false
 }
 
 export default createEslintRule<Options, MESSAGE_IDS>({
@@ -47,8 +70,30 @@ export default createEslintRule<Options, MESSAGE_IDS>({
 			CallExpression(node: TSESTree.CallExpression) {
 				if (isTestCaseCallWithCallbackArg(node, context))
 					inTestCaseWithDoneCallBack = true
+
+				if (isPromiseChainCall(node)) {
+					chains.unshift(false)
+
+					return
+				}
+
+				if (chains.length > 0 && isTypeOfVitestFnCall(node, context, ['expect']))
+					chains[0] = true
+			},
+			'CallExpression:exit'(node: TSESTree.CallExpression) {
+				if (inTestCaseWithDoneCallBack) {
+					if (isTypeOfVitestFnCall(node, context, ['test']))
+						inTestCaseWithDoneCallBack = false
+
+					return
+				}
+
+				if (isPromiseChainCall(node)) return
+
+				const hasExpectCall = chains.shift()
+
+				// TODO: start from here
 			}
-			// 'CallExpression:exit'(node: TSESTree.CallExpression) { }
 		}
 	}
 })
