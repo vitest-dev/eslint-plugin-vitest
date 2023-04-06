8647df4afaed2c3ff071876a9c2b91c662c1975e
Verité Mugabo <mugaboverite@gmail.com>
2023-04-06 10:16:53 +0200

    feat/require-top-level-describe (#141)
    * feat: require-top-level-describe
    
    * feat: added require top level describe

---
+++ docs/rules/require-top-level-describe.md
@@ -0,0 +1,62 @@
+# Enforce that all tests are in a top-level describe (`vitest/require-top-level-describe`)
+
+⚠️ This rule _warns_ in the 🌐 `all` config.
+
+<!-- end auto-generated rule header -->
+
+This rule triggers warning if a test case (`test` and `it`) or a hook (`beforeAll`, `beforeEach`, `afterEach`, `afterAll`) is not located in a top-level `describe` block.
+
+
+## Options
+
+This rule accepts an object with the following properties: 
+
+- `maxNumberOfTopLevelDescribes`: The maximum number of top-level tests allowed in a file. Defaults to `Infinity`. Allowing any number of top-level describe blocks.
+
+```json
+{
+	"vitest/require-top-level-describe": [
+		"error", 
+		{ 
+			"maxNumberOfTopLevelDescribes": 2 
+		}
+	]
+}
+```
+
+
+
+The following patterns are considered warnings:
+
+```js
+test('foo', () => {})
+
+beforeEach(() => {
+	describe('bar', () => {
+		test('baz', () => {})
+	})
+})
+
+
+```
+
+The following patterns are not considered warnings:
+
+```js
+describe('foo', () => {
+	test('bar', () => {})
+})
+
+describe('foo', () => {
+	beforeEach(() => {
+		describe('bar', () => {
+			test('baz', () => {})
+		})
+	})
+})
+
+```
+
+
+
+
---
+++ src/rules/require-top-level-describe.test.ts
@@ -0,0 +1,170 @@
+import { describe, test } from 'vitest'
+import ruleTester from '../utils/tester'
+import rule, { RULE_NAME } from './require-top-level-describe'
+
+describe(RULE_NAME, () => {
+	test(RULE_NAME, () => {
+		ruleTester.run(`${RULE_NAME}: require-top-level-describe`, rule, {
+			valid: [
+				'it.each()',
+				'describe("test suite", () => { test("my test") });',
+				'describe("test suite", () => { it("my test") });',
+				`
+					  describe("test suite", () => {
+						beforeEach("a", () => {});
+						describe("b", () => {});
+						test("c", () => {})
+					  });
+					`,
+				'describe("test suite", () => { beforeAll("my beforeAll") });',
+				'describe("test suite", () => { afterEach("my afterEach") });',
+				'describe("test suite", () => { afterAll("my afterAll") });',
+				`
+					describe("test suite", () => {
+					  it("my test", () => {})
+					  describe("another test suite", () => {
+					  });
+					  test("my other test", () => {})
+					});
+				  `,
+				'foo()',
+				'describe.each([1, true])("trues", value => { it("an it", () => expect(value).toBe(true) ); });',
+				`
+					describe('%s', () => {
+					  it('is fine', () => {
+						//
+					  });
+					});
+			  
+					describe.each('world')('%s', () => {
+					  it.each([1, 2, 3])('%n', () => {
+						//
+					  });
+					});
+				  `,
+				`
+					describe.each('hello')('%s', () => {
+					  it('is fine', () => {
+						//
+					  });
+					});
+			  
+					describe.each('world')('%s', () => {
+					  it.each([1, 2, 3])('%n', () => {
+						//
+					  });
+					});
+				  `
+			],
+			invalid: [
+				{
+					code: 'beforeEach("my test", () => {})',
+					errors: [{ messageId: 'unexpectedHook' }]
+				},
+				{
+					code: `
+						  test("my test", () => {})
+						  describe("test suite", () => {});
+						`,
+					errors: [{ messageId: 'unexpectedTestCase' }]
+				},
+				{
+					code: `
+						  test("my test", () => {})
+						  describe("test suite", () => {
+							it("test", () => {})
+						  });
+						`,
+					errors: [{ messageId: 'unexpectedTestCase' }]
+				},
+				{
+					code: `
+						  describe("test suite", () => {});
+						  afterAll("my test", () => {})
+						`,
+					errors: [{ messageId: 'unexpectedHook' }]
+				},
+				{
+					code: 'it.skip(\'test\', () => {});',
+					errors: [{ messageId: 'unexpectedTestCase' }]
+				},
+				{
+					code: 'it.each([1, 2, 3])(\'%n\', () => {});',
+					errors: [{ messageId: 'unexpectedTestCase' }]
+				},
+				{
+					code: 'it.skip.each([1, 2, 3])(\'%n\', () => {});',
+					errors: [{ messageId: 'unexpectedTestCase' }]
+				},
+				{
+					code: 'it.skip.each``(\'%n\', () => {});',
+					errors: [{ messageId: 'unexpectedTestCase' }]
+				},
+				{
+					code: 'it.each``(\'%n\', () => {});',
+					errors: [{ messageId: 'unexpectedTestCase' }]
+				}
+			]
+		})
+
+		ruleTester.run(`${RULE_NAME}: (enforce number of describe)`, rule, {
+			valid: [
+				'describe("test suite", () => { test("my test") });',
+				'foo()',
+				'describe.each([1, true])("trues", value => { it("an it", () => expect(value).toBe(true) ); });',
+				{
+					code: `
+					  describe('one', () => {
+						describe('two', () => {});
+						describe('three', () => {});
+					  });
+					`,
+					options: [{ maxNumberOfTopLevelDescribes: 1 }]
+				}
+			],
+			invalid: [
+				{
+					code: `
+					  describe('one', () => {});
+					  describe('two', () => {});
+					  describe('three', () => {});
+					`,
+					options: [{ maxNumberOfTopLevelDescribes: 2 }],
+					errors: [{ messageId: 'tooManyDescribes', line: 4 }]
+				},
+				{
+					code: `
+					  describe('one', () => {
+						describe('one (nested)', () => {});
+						describe('two (nested)', () => {});
+					  });
+					  describe('two', () => {
+						describe('one (nested)', () => {});
+						describe('two (nested)', () => {});
+						describe('three (nested)', () => {});
+					  });
+					  describe('three', () => {
+						describe('one (nested)', () => {});
+						describe('two (nested)', () => {});
+						describe('three (nested)', () => {});
+					  });
+					`,
+					options: [{ maxNumberOfTopLevelDescribes: 2 }],
+					errors: [{ messageId: 'tooManyDescribes', line: 11 }]
+				},
+				{
+					code: `
+					  describe('one', () => {});
+					  describe('two', () => {});
+					  describe('three', () => {});
+					`,
+					options: [{ maxNumberOfTopLevelDescribes: 1 }],
+					errors: [
+						{ messageId: 'tooManyDescribes', line: 3 },
+						{ messageId: 'tooManyDescribes', line: 4 }
+					]
+				}
+			]
+		})
+	})
+})
