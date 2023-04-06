90c2b62679383d131b9e171a448fe0efa28fe27a
Verité Mugabo <mugaboverite@gmail.com>
2023-04-06 10:42:45 +0200

    feat: require-to-throw-message (#142)

---
+++ docs/rules/require-to-throw-message.md
@@ -0,0 +1,40 @@
+# Require toThrow() to be called with an error message (`vitest/require-to-throw-message`)
+
+⚠️ This rule _warns_ in the 🌐 `all` config.
+
+<!-- end auto-generated rule header -->
+
+This rule triggers a warning if `toThrow()` or `toThrowError()` is used without
+an error message.
+
+The following patterns are considered warnings:
+
+```js
+test('foo', () => {
+  expect(() => {
+	throw new Error('foo')
+  }).toThrow()
+})
+
+test('foo', () => {
+  expect(() => {
+	throw new Error('foo')
+  }).toThrowError()
+})
+```
+
+The following patterns are not considered warnings:
+
+```js
+test('foo', () => {
+  expect(() => {
+	throw new Error('foo')
+  }).toThrow('foo')
+})
+
+test('foo', () => {
+  expect(() => {
+	throw new Error('foo')
+  }).toThrowError('foo')
+})
+```
---
+++ src/rules/require-to-throw-message.ts
@@ -0,0 +1,45 @@
+import { createEslintRule, getAccessorValue } from '../utils'
+import { parseVitestFnCall } from '../utils/parseVitestFnCall'
+
+export const RULE_NAME = 'require-to-throw-message'
+type MESSAGE_IDS = 'addErrorMessage'
+type Options = []
+
+export default createEslintRule<Options, MESSAGE_IDS>({
+	name: RULE_NAME,
+	meta: {
+		type: 'suggestion',
+		docs: {
+			description: 'Require toThrow() to be called with an error message',
+			recommended: 'warn'
+		},
+		schema: [],
+		messages: {
+			addErrorMessage: 'Add an error message to {{ matcherName }}()'
+		}
+	},
+	defaultOptions: [],
+	create(context) {
+		return {
+			CallExpression(node) {
+				const vitestFnCall = parseVitestFnCall(node, context)
+
+				if (vitestFnCall?.type !== 'expect') return
+
+				const { matcher } = vitestFnCall
+				const matcherName = getAccessorValue(matcher)
+
+				if (vitestFnCall.args.length === 0 &&
+					['toThrow', 'toThrowError'].includes(matcherName) &&
+					!vitestFnCall.modifiers.some(nod => getAccessorValue(nod) === 'not')
+				) {
+					context.report({
+						messageId: 'addErrorMessage',
+						data: { matcherName },
+						node: matcher
+					})
+				}
+			}
+		}
+	}
+})
