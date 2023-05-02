590e1c04c30059ef4b18c3c2aa4181438f3ad077
Drew Pereli <drewpereli@gmail.com>
2023-05-02 12:29:16 -0700

    Update no-focused-tests rule
    Allow it to catch `it.only.each`

--- src/rules/no-focused-tests.test.ts
+++ src/rules/no-focused-tests.test.ts
@@ -3,40 +3,53 @@ import { it } from 'vitest'
 import rule, { RULE_NAME } from './no-focused-tests'
 
 it(RULE_NAME, () => {
-	const ruleTester: RuleTester = new RuleTester({
-		parser: require.resolve('@typescript-eslint/parser')
-	})
+  const ruleTester: RuleTester = new RuleTester({
+    parser: require.resolve('@typescript-eslint/parser')
+  })
 
-	ruleTester.run(RULE_NAME, rule, {
-		valid: ['it("test", () => {});', 'describe("test group", () => {});'],
+  ruleTester.run(RULE_NAME, rule, {
+    valid: ['it("test", () => {});', 'describe("test group", () => {});'],
 
-		invalid: [
-			{
-				code: 'it.only("test", () => {});',
-				errors: [
-					{
-						column: 4,
-						endColumn: 8,
-						endLine: 1,
-						line: 1,
-						messageId: 'noFocusedTests'
-					}
-				],
-				output: 'it.only("test", () => {});'
-			},
-			{
-				code: 'describe.only("test", () => {});',
-				errors: [
-					{
-						column: 10,
-						endColumn: 14,
-						endLine: 1,
-						line: 1,
-						messageId: 'noFocusedTests'
-					}
-				],
-				output: 'describe.only("test", () => {});'
-			}
-		]
-	})
+    invalid: [
+      {
+        code: 'it.only("test", () => {});',
+        errors: [
+          {
+            column: 4,
+            endColumn: 8,
+            endLine: 1,
+            line: 1,
+            messageId: 'noFocusedTests'
+          }
+        ],
+        output: 'it.only("test", () => {});'
+      },
+      {
+        code: 'describe.only("test", () => {});',
+        errors: [
+          {
+            column: 10,
+            endColumn: 14,
+            endLine: 1,
+            line: 1,
+            messageId: 'noFocusedTests'
+          }
+        ],
+        output: 'describe.only("test", () => {});'
+      },
+      {
+        code: 'it.only.each([])("test", () => {});',
+        errors: [
+          {
+            column: 4,
+            endColumn: 8,
+            endLine: 1,
+            line: 1,
+            messageId: 'noFocusedTests',
+          },
+        ],
+        output: 'it.only.each([])("test", () => {});',
+      }
+    ]
+  })
 })
--- src/rules/no-focused-tests.ts
+++ src/rules/no-focused-tests.ts
@@ -1,44 +1,65 @@
 import { createEslintRule } from '../utils'
 
-export type MessageIds = 'noFocusedTests';
+export type MessageIds = 'noFocusedTests'
 export const RULE_NAME = 'no-focused-tests'
 export type Options = [];
 
 export default createEslintRule<Options, MessageIds>({
-	name: RULE_NAME,
-	meta: {
-		type: 'problem',
-		docs: {
-			description: 'Disallow focused tests',
-			recommended: 'error'
-		},
-		fixable: 'code',
-		schema: [],
-		messages: {
-			noFocusedTests: 'Focused tests are not allowed.'
-		}
-	},
-	defaultOptions: [],
-	create: (context) => {
-		return {
-			ExpressionStatement(node) {
-				if (node.expression.type === 'CallExpression') {
-					const { callee } = node.expression
-					if (
-						callee.type === 'MemberExpression' &&
+  name: RULE_NAME,
+  meta: {
+    type: 'problem',
+    docs: {
+      description: 'Disallow focused tests',
+      recommended: 'error'
+    },
+    fixable: 'code',
+    schema: [],
+    messages: {
+      noFocusedTests: 'Focused tests are not allowed.'
+    }
+  },
+  defaultOptions: [],
+  create: (context) => {
+    return {
+      ExpressionStatement(node) {
+        if (node.expression.type === 'CallExpression') {
+          const { callee } = node.expression
+          if (
+            callee.type === 'MemberExpression' &&
             callee.object.type === 'Identifier' &&
             (callee.object.name === 'it' ||
               callee.object.name === 'describe') &&
             callee.property.type === 'Identifier' &&
             callee.property.name === 'only'
-					) {
-						context.report({
-							node: callee.property,
-							messageId: 'noFocusedTests'
-						})
-					}
-				}
-			}
-		}
-	}
+          ) {
+            context.report({
+              node: callee.property,
+              messageId: 'noFocusedTests'
+            })
+          }
+
+          if (callee.type !== 'CallExpression') return;
+
+          const subCallee = callee.callee
+
+          if (
+            subCallee.type === 'MemberExpression' &&
+            subCallee.object.type === 'MemberExpression' &&
+            subCallee.object.object.type === 'Identifier' &&
+            (subCallee.object.object.name === 'it' ||
+              subCallee.object.object.name === 'describe') &&
+            subCallee.object.property.type === 'Identifier' &&
+            subCallee.object.property.name === 'only' &&
+            subCallee.property.type === 'Identifier' &&
+            subCallee.property.name === 'each'
+          ) {
+            context.report({
+              node: subCallee.object.property,
+              messageId: 'noFocusedTests'
+            })
+          }
+        }
+      }
+    }
+  }
 })
