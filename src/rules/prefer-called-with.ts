36e7e905666f939d1b59eac51a20677783216c49
Verité Mugabo <mugaboverite@gmail.com>
2023-07-09 17:06:39 +0300

    feat: add autoFix to prefer-called-with (#205)

--- src/rules/prefer-called-with.ts
+++ src/rules/prefer-called-with.ts
@@ -2,44 +2,51 @@ import { createEslintRule, getAccessorValue } from '../utils'
 import { parseVitestFnCall } from '../utils/parseVitestFnCall'
 
 export const RULE_NAME = 'prefer-called-with'
-type MESSAGE_IDS = 'preferCalledWith'
-type Options = []
+type MESSAGE_IDS = 'preferCalledWith';
+type Options = [];
 
 export default createEslintRule<Options, MESSAGE_IDS>({
-	name: RULE_NAME,
-	meta: {
-		docs: {
-			description:
-				'Suggest using `toBeCalledWith()` or `toHaveBeenCalledWith()`',
-			recommended: 'warn'
-		},
-		messages: {
-			preferCalledWith: 'Prefer {{ matcherName }}With(/* expected args */)'
-		},
-		type: 'suggestion',
-		schema: []
-	},
-	defaultOptions: [],
-	create(context) {
-		return {
-			CallExpression(node) {
-				const vitestFnCall = parseVitestFnCall(node, context)
+  name: RULE_NAME,
+  meta: {
+    docs: {
+      description:
+        'Suggest using `toBeCalledWith()` or `toHaveBeenCalledWith()`',
+      recommended: 'warn'
+    },
+    messages: {
+      preferCalledWith: 'Prefer {{ matcherName }}With(/* expected args */)'
+    },
+    type: 'suggestion',
+	fixable: 'code',
+    schema: []
+  },
+  defaultOptions: [],
+  create(context) {
+    return {
+      CallExpression(node) {
+        const vitestFnCall = parseVitestFnCall(node, context)
 
-				if (vitestFnCall?.type !== 'expect') return
+        if (vitestFnCall?.type !== 'expect') return
 
-				if (vitestFnCall.modifiers.some(node => getAccessorValue(node) === 'not')) return
+        if (
+          vitestFnCall.modifiers.some(
+            (node) => getAccessorValue(node) === 'not'
+          )
+        )
+          return
 
-				const { matcher } = vitestFnCall
-				const matcherName = getAccessorValue(matcher)
+        const { matcher } = vitestFnCall
+        const matcherName = getAccessorValue(matcher)
 
-				if (['toBeCalled', 'toHaveBeenCalled', 'toHaveBeenCalledOnce'].includes(matcherName)) {
-					context.report({
-						data: { matcherName },
-						messageId: 'preferCalledWith',
-						node: matcher
-					})
-				}
-			}
-		}
-	}
+        if (['toBeCalled', 'toHaveBeenCalled', 'toHaveBeenCalledOnce'].includes(matcherName)) {
+          context.report({
+            data: { matcherName },
+            messageId: 'preferCalledWith',
+            node: matcher,
+			fix: (fixer) => [fixer.replaceText(matcher, `${matcherName}With`)]
+          })
+        }
+      }
+    }
+  }
 })
