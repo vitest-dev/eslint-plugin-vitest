1367fcbf3167aaa6fa832938e4ca8124e79d93d0
Verite Mugabo <mugaboverite@gmail.com>
2023-07-14 10:07:30 +0200

    feat: update to latest version

--- src/rules/unbound-method.test.ts
+++ src/rules/unbound-method.test.ts
@@ -1,15 +1,32 @@
-import { ESLintUtils } from '@typescript-eslint/utils'
-import unboundMethod from './unbound-method'
+import { ESLintUtils } from "@typescript-eslint/utils";
+import unboundMethod from "./unbound-method";
 
 const ruleTester = new ESLintUtils.RuleTester({
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		sourceType: 'module',
-		project: './tsconfig.json'
-	}
-})
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+});
 
-ruleTester.run('unbound-method', unboundMethod, {
-	valid: [],
-	invalid: []
-})
+ruleTester.run("unbound-method", unboundMethod, {
+  valid: [
+    `class MyClass {
+		public logArrowBound = (): void => {
+			console.log(bound);
+		};
+	
+		public logManualBind(): void {
+			console.log(this);
+		}
+	}
+	
+	const instance = new MyClass();
+	const logArrowBound = instance.logArrowBound;
+	const logManualBind = instance.logManualBind.bind(instance);
+	
+	logArrowBound();
+	logManualBind();`,
+  ],
+  invalid: [],
+});
