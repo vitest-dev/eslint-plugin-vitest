d0e882e2dbbe9a4b232918fb3a83527f2a55ece4
Verite Mugabo <mugaboverite@gmail.com>
2023-12-08 18:54:27 -0500

    chore(packages): update vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,4 @@
 import { defineConfig, configDefaults } from 'vitest/config'
-import * as vitest from 'vitest'
-import { RuleTester } from '@typescript-eslint/rule-tester'
-
-RuleTester.afterAll = vitest.afterAll
-RuleTester.it = vitest.it
-RuleTester.describe = vitest.describe
 
 export default defineConfig({
 	test: {
