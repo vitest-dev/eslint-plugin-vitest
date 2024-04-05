099c37eabec1a9e42844d00f4462b974c972e5b6
Verite Mugabo <mugaboverite@gmail.com>
2024-04-05 19:54:33 -0400

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -205,12 +205,14 @@ const plugin = {
 
 Object.assign(plugin.configs, {
     recommended: {
+        plugins: ['vitest'],
         rules: createConfig(recommended)
     }
 })
 
 Object.assign(plugin.configs, {
     all: {
+        plugins: ['vitest'],
         rules: createConfig(allRules)
     }
 })
