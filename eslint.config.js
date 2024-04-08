eceb58af6f91e2c3f92d4655a75a7d8307b9872a
Verite Mugabo <mugaboverite@gmail.com>
2024-04-08 13:41:55 -0400

    chore(update-configs): eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,18 @@
-// @ts-check
-
-import eslint from '@eslint/js'
-import eslintPlugin from 'eslint-plugin-eslint-plugin'
-import nodePlugin from 'eslint-plugin-n'
-import stylistic from '@stylistic/eslint-plugin'
-import tseslint from 'typescript-eslint'
-import babelParser from '@babel/eslint-parser'
+import vitest from 'eslint-plugin-vitest'
 
 export default [
   {
-    languageOptions: {
-      parser: babelParser,
-      parserOptions: {
-        requireConfigFile: false,
-        sourceType: 'module',
-        babelOptions: {
-          babelrc: false,
-          configFile: false,
-          presets: ['@babel/preset-env'],
-        },
-      },
+    files: ['tests/**'], // or any other pattern
+    plugins: {
+      vitest
     },
-  },
-  ...tseslint.config(eslint.configs.recommended,
-    ...tseslint.configs.recommended),
-  eslintPlugin.configs['flat/recommended'],
-  nodePlugin.configs['flat/recommended-script'],
-  stylistic.configs.customize({
-    quotes: 'single',
-    semi: false,
-    commaDangle: 'always-multiline',
-  }),
+    rules: {
+      ...vitest.configs.recommended.rules
+    },
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals
+      }
+    }
+  }
 ]
