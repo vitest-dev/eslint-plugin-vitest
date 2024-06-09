cc4092c41315775f680b5b55bbbf3c6cae2a1f31
Verite Mugabo <mugaboverite@gmail.com>
2024-06-09 10:35:29 -0400

    feat(update): fix remote tester

--- eslint-remote-tester.config.ts
+++ eslint-remote-tester.config.ts
@@ -1,31 +1,10 @@
-import type { Config } from 'eslint-remote-tester'
-import { getPathIgnorePattern, getRepositories } from 'eslint-remote-tester-repositories'
+import vitest from "eslint-plugin-vitest"
 
-const config: Config = {
-  repositories: getRepositories(),
-  pathIgnorePattern: getPathIgnorePattern(),
-  extensions: ['js', 'jsx', 'ts', 'tsx', 'cts', 'mts'],
-
+export default {
+  repositories: ['AriPerkkio/eslint-remote-tester-integration-test-target'],
+  extensions: ['ts', 'tsx', 'cts', 'mts'],
   concurrentTasks: 3,
   cache: false,
   logLevel: 'info',
-
-  eslintrc: {
-    root: true,
-    env: { es6: true },
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      ecmaFeatures: { jsx: true }
-    },
-    overrides: [
-      {
-        files: ['*.ts', '*.tsx', '*.mts', '*.cts'],
-        parser: '@typescript-eslint/parser'
-      }
-    ],
-    extends: ['plugin:vitest/legacy-all']
-  }
+  eslintConfig: [vitest.configs.all]
 }
-
-export default config
