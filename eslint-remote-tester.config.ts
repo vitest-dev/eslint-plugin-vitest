c8e9e9cb6f52a7c59cfeb66adfe7ea0889e7731a
Ari Perkkiö <ari.perkkio@gmail.com>
2024-06-12 19:19:01 +0300

    test: fix smoke tests (#474)
    * fix: `require` in ESM
    
    * chore: set `packageManager`
    
    * test: fix smoke tests

--- eslint-remote-tester.config.ts
+++ eslint-remote-tester.config.ts
@@ -1,10 +1,24 @@
-import vitest from "eslint-plugin-vitest"
+import parser from '@typescript-eslint/parser';
+import type { Config } from 'eslint-remote-tester';
+import {
+  getPathIgnorePattern,
+  getRepositories,
+} from 'eslint-remote-tester-repositories';
+import vitest from './dist/index.mjs';
 
 export default {
-  repositories: ['AriPerkkio/eslint-remote-tester-integration-test-target'],
+  repositories: getRepositories(),
+  pathIgnorePattern: getPathIgnorePattern(),
   extensions: ['ts', 'tsx', 'cts', 'mts'],
   concurrentTasks: 3,
   cache: false,
   logLevel: 'info',
-  eslintConfig: [vitest.configs.all]
-}
+  eslintConfig: [
+    vitest.configs.all,
+    {
+      languageOptions: {
+        parser,
+      },
+    },
+  ] as Config['eslintConfig'],
+} satisfies Config;
