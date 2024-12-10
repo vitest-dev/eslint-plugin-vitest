11380035421baa676433854964fcec4b694f68ee
Yukihiro Hasegawa <49516827+y-hsgw@users.noreply.github.com>
2024-12-11 01:13:54 +0900

    fix: padding-around-expect-groups doesn't detect expectTypeOf as an expect clause (#588)
    * fix: support expectTypeOf in padding-around-expect-groups rule
    
    * test: add test

--- src/rules/padding-around-expect-groups.ts
+++ src/rules/padding-around-expect-groups.ts
@@ -18,6 +18,21 @@ export const config: Config[] = [
     prevStatementType: StatementType.ExpectToken,
     nextStatementType: StatementType.ExpectToken,
   },
+  {
+    paddingType: PaddingType.Always,
+    prevStatementType: StatementType.Any,
+    nextStatementType: StatementType.ExpectTypeOfToken,
+  },
+  {
+    paddingType: PaddingType.Always,
+    prevStatementType: StatementType.ExpectTypeOfToken,
+    nextStatementType: StatementType.Any,
+  },
+  {
+    paddingType: PaddingType.Any,
+    prevStatementType: StatementType.ExpectTypeOfToken,
+    nextStatementType: StatementType.ExpectTypeOfToken,
+  },
 ];
 
 export default createPaddingRule(
