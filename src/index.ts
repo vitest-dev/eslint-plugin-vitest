6cb1374ad43ca3d6de05f14f3e6c75c11fe620d8
Verite Mugabo <mugaboverite@gmail.com>
2025-02-12 08:17:17 -0500

    fix: remove conflict with another rule

--- src/index.ts
+++ src/index.ts
@@ -73,8 +73,8 @@ const createConfig = <R extends Linter.RulesRecord>(rules: R) => (
       [`vitest/${ruleName}`]: rules[ruleName]
     }
   }, {})) as {
-  [K in keyof R as `vitest/${Extract<K, string>}`]: R[K]
-}
+    [K in keyof R as `vitest/${Extract<K, string>}`]: R[K]
+  }
 
 const createConfigLegacy = (rules: Record<string, string>) => ({
   plugins: ['@vitest'],
@@ -149,7 +149,7 @@ const allRules = {
   [validDescribeCallbackName]: 'warn',
   [requireLocalTestContextForConcurrentSnapshotsName]: 'warn',
   [noImportNodeTestName]: 'warn',
-  [preferStrictBooleanMatchersName]: 'warn',
+  [preferStrictBooleanMatchersName]: 'off',
   [requireMockTypeParametersName]: 'warn'
 } as const
 
