97dcb0ff4e8a67ef17ee86c5309b49eda75b8bcb
Marco Comes <comes95@gmail.com>
2025-02-27 13:53:07 +0100

    fix(consistent-test-filename): match allTestPattern against full file path (#674)

--- tests/consistent-test-filename.test.ts
+++ tests/consistent-test-filename.test.ts
@@ -15,6 +15,14 @@ ruleTester.run(`file-name`, rule, {
       filename: '1.spec.ts',
       errors: [{ messageId: 'consistentTestFilename' }],
       options: [{ pattern: String.raw`.*\.test\.ts$` }]
-    }
+    },
+    {
+      code: "export {}",
+      filename: "__tests__/2.ts",
+      errors: [{ messageId: "consistentTestFilename" }],
+      options: [
+        { allTestPattern: String.raw`__tests__`, pattern: String.raw`.*\.test\.ts$` },
+      ],
+    },
   ]
 })
