fe3eed038ad0be08801e55dcd40e6de83fe3ed20
Verite Mugabo <mugaboverite@gmail.com>
2025-06-26 16:39:50 -0400

    chore: improve documentation

--- src/index.ts
+++ src/index.ts
@@ -83,11 +83,11 @@ const createConfig = <R extends Linter.RulesRecord>(rules: R) =>
   }
 
 const createConfigLegacy = (rules: Record<string, string>) => ({
-  plugins: ['vitest'],
+  plugins: ['@vitest'],
   rules: Object.keys(rules).reduce((acc, ruleName) => {
     return {
       ...acc,
-      [`vitest/${ruleName}`]: rules[ruleName],
+      [`@vitest/${ruleName}`]: rules[ruleName],
     }
   }, {}),
 })
