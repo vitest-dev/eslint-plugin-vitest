703b00668bdeb1e2f8305e3f09d8b639efeee043
Evyatar Daud <32631382+StyleShit@users.noreply.github.com>
2026-03-12 17:15:00 +0200

    fix(no-focused-tests): not all chainable APIs are reported (#878)
    Closes #877

--- src/rules/no-focused-tests.ts
+++ src/rules/no-focused-tests.ts
@@ -1,5 +1,5 @@
-import { TSESTree } from '@typescript-eslint/utils'
-import { createEslintRule } from '../utils'
+import { createEslintRule, getAccessorValue } from '../utils'
+import { parseVitestFnCall } from '../utils/parse-vitest-fn-call'
 
 export type MessageIds = 'noFocusedTests'
 const RULE_NAME = 'no-focused-tests'
@@ -8,16 +8,6 @@ export type Options = [
     fixable: boolean
   }>,
 ]
-
-const isTestOrDescribe = (node: TSESTree.Expression) => {
-  return (
-    node.type === 'Identifier' && ['it', 'test', 'describe'].includes(node.name)
-  )
-}
-
-const isOnly = (node: TSESTree.Expression | TSESTree.PrivateIdentifier) => {
-  return node.type === 'Identifier' && node.name === 'only'
-}
 
 export default createEslintRule<Options, MessageIds>({
   name: RULE_NAME,
@@ -49,78 +39,30 @@ export default createEslintRule<Options, MessageIds>({
     const fixable = options[0].fixable!
 
     return {
-      ExpressionStatement(node) {
-        if (node.expression.type === 'CallExpression') {
-          const { callee } = node.expression
-          if (
-            callee.type === 'MemberExpression' &&
-            isTestOrDescribe(callee.object) &&
-            isOnly(callee.property)
-          ) {
-            context.report({
-              node: callee.property,
-              messageId: 'noFocusedTests',
-              fix: (fixer) =>
-                fixable
-                  ? fixer.removeRange([
-                      callee.property.range[0] - 1,
-                      callee.property.range[1],
-                    ])
-                  : null,
-            })
-          }
-
-          if (callee.type === 'TaggedTemplateExpression') {
-            const tagCall =
-              callee.tag.type === 'MemberExpression' ? callee.tag.object : null
-            if (!tagCall) return
-
-            if (
-              tagCall.type === 'MemberExpression' &&
-              isTestOrDescribe(tagCall.object) &&
-              isOnly(tagCall.property)
-            ) {
-              context.report({
-                node: tagCall.property,
-                messageId: 'noFocusedTests',
-                fix: (fixer) =>
-                  fixable
-                    ? fixer.removeRange([
-                        tagCall.property.range[0] - 1,
-                        tagCall.property.range[1],
-                      ])
-                    : null,
-              })
-            }
-          }
-        }
-      },
       CallExpression(node) {
-        if (node.callee.type === 'CallExpression') {
-          const { callee } = node.callee
+        const vitestFnCall = parseVitestFnCall(node, context)
 
-          if (
-            callee.type === 'MemberExpression' &&
-            callee.object.type === 'MemberExpression' &&
-            isTestOrDescribe(callee.object.object) &&
-            isOnly(callee.object.property) &&
-            callee.property.type === 'Identifier' &&
-            callee.property.name === 'each'
-          ) {
-            const onlyCallee = callee.object.property
+        if (!vitestFnCall) {
+          return
+        }
 
-            context.report({
-              node: callee.object.property,
-              messageId: 'noFocusedTests',
-              fix: (fixer) =>
-                fixable
-                  ? fixer.removeRange([
-                      onlyCallee.range[0] - 1,
-                      onlyCallee.range[1],
-                    ])
-                  : null,
-            })
-          }
+        const isTestOrDescribe = ['it', 'test', 'describe'].includes(
+          vitestFnCall.name,
+        )
+
+        const onlyNode = vitestFnCall.members.find(
+          (m) => getAccessorValue(m) === 'only',
+        )
+
+        if (isTestOrDescribe && onlyNode) {
+          context.report({
+            node: onlyNode,
+            messageId: 'noFocusedTests',
+            fix: (fixer) =>
+              fixable
+                ? fixer.removeRange([onlyNode.range[0] - 1, onlyNode.range[1]])
+                : null,
+          })
         }
       },
     }
